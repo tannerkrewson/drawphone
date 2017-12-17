fbc753f802036fdacd7dcb5318a0a36fe654043b
tannerkrewson <tannerkrewson@gmail.com>
2017-12-17 16:59:04 -0600

    Remove unused favicon stuffs

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var socketio = require('socket.io');
 var path = require('path');
-//var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -22,8 +21,6 @@ require('./routes')(app);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 if (devModeEnabled) {
 	app.use(logger('dev'));
 } else {

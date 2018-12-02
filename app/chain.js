aac215292fb96591862f21c3e2394d848d91fb29
meshiest <meek.mesh@gmail.com>
2018-12-01 19:18:57 -0500

    Updated game.js, removed newline in chain.js

--- app/chain.js
+++ app/chain.js
@@ -71,5 +71,4 @@ class Chain {
 }
 
 
-
 module.exports = Chain;
--- app/game.js
+++ app/game.js
@@ -2,164 +2,149 @@
 // Drawphone Game
 //
 
-var Round = require('./round');
-var Player = require('./player');
+const Round = require('./round');
+const Player = require('./player');
 
-function Game(code, onEmpty) {
-	this.code = code;
-	this.onEmpty = onEmpty;
-	this.players = [];
-	this.admin;
-	this.inProgress = false;
-	this.currentRound;
+class Game {
+	constructor(code, onEmpty) {
+		this.code = code;
+		this.onEmpty = onEmpty;
+		this.players = [];
+		this.admin;
+		this.inProgress = false;
+		this.currentRound;
 
-	this.currentId = 1;
-	this.currentRoundNum = 1;
-}
-
-Game.prototype.newPlayer = function (name, socket) {
-	return new Player(name, socket, this.getNextId());
-};
-
-Game.prototype.addPlayer = function (name, socket) {
-	var newPlayer = this.newPlayer(name, socket);
-	this.initPlayer(newPlayer);
-	this.players.push(newPlayer);
-	this.sendUpdatedPlayersList();
-	return newPlayer;
-};
-
-Game.prototype.initPlayer = function (newPlayer) {
-	//if this is the first user, make them admin
-	if (this.players.length === 0) {
-		this.admin = newPlayer;
-		newPlayer.makeAdmin();
+		this.currentId = 1;
+		this.currentRoundNum = 1;
 	}
 
-	//when this player disconnects, remove them from this game
-	var self = this;
-	newPlayer.socket.on('disconnect', function () {
-		newPlayer.isConnected = false;
-		if (self.inProgress) {
-			self.currentRound.findReplacementFor(newPlayer);
-		} else {
-			self.removePlayer(newPlayer.id);
-		}
-		self.onPlayerDisconnect(newPlayer);
-		self.sendUpdatedPlayersList();
-	});
+	newPlayer(name, socket) {
+		return new Player(name, socket, this.getNextId());
+	}
 
-	newPlayer.socket.on('viewPreviousResults', function () {
-		if (self.currentRound && self.currentRound.canViewLastRoundResults) {
-			newPlayer.send('viewResults', {
-				chains: self.currentRound.getAllChains()
-			});
-		}
-	});
-};
+	addPlayer(name, socket) {
+		const newPlayer = this.newPlayer(name, socket);
+		this.initPlayer(newPlayer);
+		this.players.push(newPlayer);
+		this.sendUpdatedPlayersList();
+		return newPlayer;
+	}
 
-Game.prototype.onPlayerDisconnect = function (oldPlayer) {
-	//if the player was admin
-	if (oldPlayer.id === this.admin.id) {
-		//find the first connected player to be admin
-		for (var i = 0; i < this.players.length; i++) {
-			var thisPlayer = this.players[i];
-			if (thisPlayer.isConnected) {
-				this.admin = thisPlayer;
-				thisPlayer.makeAdmin();
-				break;
+	initPlayer(newPlayer) {
+		//if this is the first user, make them admin
+		if (this.players.length === 0) {
+			this.admin = newPlayer;
+			newPlayer.makeAdmin();
+		}
+
+		//when this player disconnects, remove them from this game
+		newPlayer.socket.on('disconnect', () => {
+			newPlayer.isConnected = false;
+			if (this.inProgress) {
+				this.currentRound.findReplacementFor(newPlayer);
+			} else {
+				this.removePlayer(newPlayer.id);
+			}
+			this.onPlayerDisconnect(newPlayer);
+			this.sendUpdatedPlayersList();
+		});
+
+		newPlayer.socket.on('viewPreviousResults', () => {
+			if (this.currentRound && this.currentRound.canViewLastRoundResults) {
+				newPlayer.send('viewResults', {
+					chains: this.currentRound.getAllChains()
+				});
+			}
+		});
+	}
+
+	onPlayerDisconnect(oldPlayer) {
+		//if the player was admin
+		if (oldPlayer.id === this.admin.id) {
+			//find the first connected player to be admin
+			for (let thisPlayer of this.players) {
+				if (thisPlayer.isConnected) {
+					this.admin = thisPlayer;
+					thisPlayer.makeAdmin();
+					break;
+				}
 			}
 		}
-	}
 
-	var allPlayersDisconnected = true;
-	for (var j = 0; j < this.players.length; j++) {
-		if (this.players[j].isConnected) {
-			allPlayersDisconnected = false;
-			break;
+		const allPlayersDisconnected = !this.players.some(player => player.isConnected);
+
+		if (allPlayersDisconnected) {
+			this.onEmpty();
 		}
 	}
-	if (allPlayersDisconnected) {
-		this.onEmpty();
-	}
-};
 
-Game.prototype.removePlayer = function (id) {
-	var player = this.getPlayer(id);
+	removePlayer(id) {
+		const player = this.getPlayer(id);
 
-	var index = this.players.indexOf(player);
-	if (index > -1) {
-		this.players.splice(index, 1);
-	}
+		const index = this.players.indexOf(player);
+		if (index > -1) {
+			this.players.splice(index, 1);
+		}
 
-	//if there are no players left
-	if (this.players.length === 0) {
-		this.onEmpty();
-	}
-};
-
-Game.prototype.getPlayer = function (id) {
-	for (var i = 0; i < this.players.length; i++) {
-		if (this.players[i].id === id) {
-			return this.players[i];
+		//if there are no players left
+		if (this.players.length === 0) {
+			this.onEmpty();
 		}
 	}
-	return false;
-};
 
-Game.prototype.getNextId = function () {
-	return this.currentId++;
-};
+	getPlayer(id) {
+		this.players.find(player => player.id === id) || false;
+	}
 
-Game.prototype.getNextRoundNum = function () {
-	return this.currentRoundNum++;
-};
+	getNextId() {
+		return this.currentId++;
+	}
 
-Game.prototype.getJsonGame = function () {
-	var players = [];
-	this.players.forEach(function (player) {
-		players.push(player.getJson());
-	});
+	getNextRoundNum() {
+		return this.currentRoundNum++;
+	}
 
-	var jsonGame = {
-		code: this.code,
-		players,
-		inProgress: this.inProgress,
-		canViewLastRoundResults: (this.currentRound !== undefined) && this.currentRound.canViewLastRoundResults
-	};
-	return jsonGame;
-};
+	getJsonGame() {
+		const players = this.players.map(player => player.getJson());
 
-Game.prototype.sendUpdatedPlayersList = function () {
-	this.sendToAll('updatePlayerList', {
-		players: this.getJsonGame().players,
-		canViewLastRoundResults: (this.currentRound !== undefined) && this.currentRound.canViewLastRoundResults
-	});
-};
+		const jsonGame = {
+			code: this.code,
+			players,
+			inProgress: this.inProgress,
+			canViewLastRoundResults: (typeof this.currentRound !== 'undefined') && this.currentRound.canViewLastRoundResults
+		};
+		return jsonGame;
+	}
 
-Game.prototype.sendToAll = function (event, data) {
-	var self = this;
-	this.players.forEach(function (player) {
-		player.socket.emit(event, {
-			success: true,
-			gameCode: self.code,
-			player: player.getJson(),
-			data
+	sendUpdatedPlayersList() {
+		this.sendToAll('updatePlayerList', {
+			players: this.getJsonGame().players,
+			canViewLastRoundResults: (typeof this.currentRound !== 'undefined') && this.currentRound.canViewLastRoundResults
 		});
-	});
-};
+	}
 
-Game.prototype.startNewRound = function (timeLimit, wordPackName) {
-	this.inProgress = true;
+	sendToAll(event, data) {
+		this.players.forEach(player => {
+			player.socket.emit(event, {
+				success: true,
+				gameCode: this.code,
+				player: player.getJson(),
+				data
+			});
+		});
+	}
 
-	var self = this;
-	this.currentRound = new Round(this.getNextRoundNum(), this.players, timeLimit, wordPackName, function () {
-		//ran when results are sent
-		self.inProgress = false;
-		self.sendUpdatedPlayersList(); //this makes sure the View Last Round Results button shows up
-	});
+	startNewRound(timeLimit, wordPackName) {
+		this.inProgress = true;
 
-	this.currentRound.start();
-};
+		this.currentRound = new Round(this.getNextRoundNum(), this.players, timeLimit, wordPackName, () => {
+			//ran when results are sent
+			this.inProgress = false;
+			this.sendUpdatedPlayersList(); //this makes sure the View Last Round Results button shows up
+		});
+
+		this.currentRound.start();
+	}
+}
 
 module.exports = Game;

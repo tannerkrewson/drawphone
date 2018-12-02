44ff660f47990017f103fcc467c7fa1a47aca45f
meshiest <meek.mesh@gmail.com>
2018-12-01 19:08:07 -0500

    Updated drawphone.js

--- app/drawphone.js
+++ app/drawphone.js
@@ -2,67 +2,55 @@
 // Drawphone
 //
 
-var Game = require('./game');
+const Game = require('./game');
 
-function Drawphone(devModeEnabled) {
-	this.games = [];
+class Drawphone {
+	constructor(devModeEnabled) {
+		this.games = [];
 
-	//add the dev game
-	if (devModeEnabled) {
-		this.newGame('ffff');
+		//add the dev game
+		if (devModeEnabled) {
+			this.newGame('ffff');
+		}
+	}
+
+	newGame(forceCode) {
+		const newCode = forceCode || this.generateCode();
+		const newGame = new Game(newCode, () => this.removeGame(newCode));
+
+		console.log(newCode + ' created');
+		this.games.push(newGame);
+
+		return newGame;
+	}
+	
+	findGame(code) {
+		return this.games.find(game => game.code === code.toLowerCase()) || false;
+	}
+
+	generateCode() {
+		const possible = 'abcdefghijklmnopqrstuvwxyz';
+		let code;
+		do {
+			//generate 4 letter code
+			code = '';
+			for (let i = 0; i < 4; i++) {
+				code += possible[Math.floor(Math.random() * possible.length)];
+			}
+			//make sure the code is not already in use
+		} while (this.findGame(code));
+		return code;
+	}
+	
+	removeGame(code) {
+		const game = this.findGame(code);
+
+		const index = this.games.indexOf(game);
+		if (index > -1) {
+			this.games.splice(index, 1);
+			console.log(code + ' removed');
+		}
 	}
 }
-
-Drawphone.prototype.newGame = function (forceCode) {
-
-	var newCode;
-	if (forceCode) {
-		newCode = forceCode;
-	} else {
-		newCode = this.generateCode();
-	}
-
-	var self = this;
-	var newGame = new Game(newCode, function () {
-		//will be ran when this game has 0 players left
-		self.removeGame(newCode);
-	});
-	this.games.push(newGame);
-	console.log(newCode + ' created');
-	return newGame;
-};
-
-Drawphone.prototype.findGame = function (code) {
-	for (var i = 0; i < this.games.length; i++) {
-		if (this.games[i].code === code.toLowerCase()) {
-			return this.games[i];
-		}
-	}
-	return false;
-};
-
-Drawphone.prototype.generateCode = function () {
-	var code;
-	do {
-		//generate 4 letter code
-		code = '';
-		var possible = 'abcdefghijklmnopqrstuvwxyz';
-		for (var i = 0; i < 4; i++) {
-			code += possible.charAt(Math.floor(Math.random() * possible.length));
-		}
-		//make sure the code is not already in use
-	} while (this.findGame(code));
-	return code;
-};
-
-Drawphone.prototype.removeGame = function (code) {
-	var game = this.findGame(code);
-
-	var index = this.games.indexOf(game);
-	if (index > -1) {
-		this.games.splice(index, 1);
-		console.log(code + ' removed');
-	}
-};
 
 module.exports = Drawphone;

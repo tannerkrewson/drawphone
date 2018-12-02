c89c3e50c946e0440dc753d78ad839372886d33a
meshiest <meek.mesh@gmail.com>
2018-12-01 19:20:54 -0500

    Updated player.js

--- app/chain.js
+++ app/chain.js
@@ -70,5 +70,4 @@ class Chain {
 	}
 }
 
-
 module.exports = Chain;
--- app/player.js
+++ app/player.js
@@ -2,37 +2,39 @@
 // Drawphone Player
 //
 
-function Player(name, socket, id) {
-	this.name = name;
-	this.socket = socket;
-	this.id = id;
-	this.isAdmin = false;
-	this.isConnected = true;
+class Player {
+	constructor(name, socket, id) {
+		this.name = name;
+		this.socket = socket;
+		this.id = id;
+		this.isAdmin = false;
+		this.isConnected = true;
+	}
+
+	getJson() {
+		return {
+			name: this.name,
+			id: this.id,
+			isAdmin: this.isAdmin,
+			isConnected: this.isConnected
+		};
+	}
+
+	send(event, data) {
+		this.socket.emit(event, {
+			you: this.getJson(),
+			data
+		});
+	}
+
+	sendThen(event, data, onEvent, next) {
+		this.send(event, data);
+		this.socket.once(onEvent, next);
+	}
+
+	makeAdmin() {
+		this.isAdmin = true;
+	}
 }
-
-Player.prototype.getJson = function () {
-	return {
-		name: this.name,
-		id: this.id,
-		isAdmin: this.isAdmin,
-		isConnected: this.isConnected
-	};
-};
-
-Player.prototype.send = function (event, data) {
-	this.socket.emit(event, {
-		you: this.getJson(),
-		data
-	});
-};
-
-Player.prototype.sendThen = function (event, data, onEvent, next) {
-	this.send(event, data);
-	this.socket.once(onEvent, next);
-};
-
-Player.prototype.makeAdmin = function () {
-	this.isAdmin = true;
-};
 
 module.exports = Player;

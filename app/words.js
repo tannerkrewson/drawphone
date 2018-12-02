09bfdbc6abc4483cc32da4d5cc6f9fdf0441d60d
meshiest <meek.mesh@gmail.com>
2018-12-01 19:53:52 -0500

    Update words.js

--- app/words.js
+++ app/words.js
@@ -2,83 +2,68 @@
 // Drawphone Words
 //
 
-var fs = require('fs');
-var path = require('path');
+const fs = require('fs');
+const path = require('path');
 
-function WordPacks () {
-	this.wordPacks = [];
-}
+class WordPacks {
+	constructor() {
+		this.wordPacks = [];
+	}
 
-WordPacks.prototype.loadAll = function () {
-	var self = this;
-	//reads each txt file in the words folder
-	//the name of the file will be the name of the pack
-	fs.readdirSync(path.join(__dirname, '../words')).forEach(function(file) {
-		//removes the .txt from the end of the filename
-		var packName = file.substring(0, file.length - 4);
+	loadAll() {
+		//reads each txt file in the words folder
+		//the name of the file will be the name of the pack
+		const files = fs.readdirSync(path.join(__dirname, '../words'));
+		for(let file of files) {
+			//removes the .txt from the end of the filename
+			const packName = path.parse(file).name;
 
-		var pathToTxt = __dirname + '/../words/' + file;
-		var arrayOfWords = fs.readFileSync(pathToTxt).toString().split(/\r?\n/);
+			const pathToTxt = __dirname + '/../words/' + file;
+			const arrayOfWords = fs.readFileSync(pathToTxt)
+				.toString()
+				.split(/\r?\n/)
+				.map(word => word.trim()) //trim all the words
+				.filter(word => word !== ''); //remove all blank lines
 
-		//remove all blank lines
-		var newArrayOfWords = [];
-		for (var i = 0; i < arrayOfWords.length; i++) {
-			var thisWord = arrayOfWords[i].trim();
-			if (thisWord !== '') {
-				newArrayOfWords.push(thisWord);
+			//can't have an empty list!
+			if (arrayOfWords.length === 0) {
+				console.log(packName + '.txt is empty. Please add some words, or delete it.');
+				process.exit(1);
 			}
-		}
-		arrayOfWords = newArrayOfWords;
 
-		//can't have an empty list!
-		if (arrayOfWords.length === 0) {
-			console.log(packName + '.txt is empty. Please add some words, or delete it.');
-			process.exit(1);
-		}
-
-		self.wordPacks.push(new WordPack(packName, arrayOfWords));
-	});
-};
-
-WordPacks.prototype.get = function (packName) {
-	for (var i = 0; i < this.wordPacks.length; i++) {
-		var thisPack = this.wordPacks[i];
-		if (thisPack.name === packName) {
-			return thisPack;
+			this.wordPacks.push(new WordPack(packName, arrayOfWords));
 		}
 	}
-	return false;
-};
 
-WordPacks.prototype.getRandomWord = function (packName) {
-	var thisPack = this.get(packName);
-	if (thisPack) {
-		return thisPack.getRandomWord();
-	} else {
-		console.error('Wordpack ' + packName + ' does not exist.');
-		return this.wordPacks[0].getRandomWord();
+	get(packName) {
+		return this.wordPacks.find(pack => pack.name === packName) || false;
 	}
-};
 
-WordPacks.getAllPackNames = function () {
-	var names = [];
-	fs.readdirSync(path.join(__dirname, '../words')).forEach(function(file) {
-		//removes the .txt from the end of the filename
-		var packName = file.substring(0, file.length - 4);
-		names.push(packName);
-	});
-	return names;
-};
-
-
-function WordPack (name, words) {
-	this.name = name;
-	this.words = words;
+	getRandomWord(packName) {
+		const thisPack = this.get(packName);
+		if (thisPack) {
+			return thisPack.getRandomWord();
+		} else {
+			console.error(`Wordpack ${packName} does not exist.`);
+			return this.wordPacks[0].getRandomWord();
+		}
+	}
 }
 
-WordPack.prototype.getRandomWord = function () {
-	return this.words[Math.floor(Math.random() * this.words.length)];
+WordPacks.getAllPackNames = () => {
+	return fs.readdirSync(path.join(__dirname, '../words'))
+		.map(file => path.parse(file).name);
 };
 
+class WordPack {
+	constructor(name, words) {
+		this.name = name;
+		this.words = words;
+	}
+
+	getRandomWord() {
+		return this.words[Math.floor(Math.random() * this.words.length)];
+	}
+}
 
 module.exports = WordPacks;

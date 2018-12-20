5b5cc5b9971c509c00d611982b67d9d495e513f0
tannerkrewson <tannerkrewson@gmail.com>
2018-12-19 22:18:23 -0600

    fix bad hostname

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 eval "$(ssh-agent -s)"
 chmod 600 .travis/deploy.key
 ssh-add .travis/deploy.key
-ssh-keyscan dokku@do.tannerkrewson.com >> ~/.ssh/known_hosts
+ssh-keyscan do.tannerkrewson.com >> ~/.ssh/known_hosts
 git remote add deploy dokku@do.tannerkrewson.com:drawphone
 git config --global push.default simple
 git push deploy prod

bacf5b7857075fb6ef63d4505b5f6bd64299774c
tannerkrewson <tannerkrewson@gmail.com>
2018-12-19 21:59:52 -0600

    test prod deploy

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-if [ "$TRAVIS_BRANCH" == "prod" ]; then
-  eval "$(ssh-agent -s)"
-  chmod 600 .travis/deploy.key
-  ssh-add .travis/deploy.key
-  ssh-keyscan dokku@do.tannerkrewson.com >> ~/.ssh/known_hosts
-  git remote add deploy dokku@do.tannerkrewson.com:drawphone
-  git config --global push.default simple
-  git push deploy prod
-fi
+# if [ "$TRAVIS_BRANCH" == "prod" ]; then
+eval "$(ssh-agent -s)"
+chmod 600 .travis/deploy.key
+ssh-add .travis/deploy.key
+ssh-keyscan dokku@do.tannerkrewson.com >> ~/.ssh/known_hosts
+git remote add deploy dokku@do.tannerkrewson.com:drawphone
+git config --global push.default simple
+git push deploy prod
+# fi

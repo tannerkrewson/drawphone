6959a1a55d1b0ccbcbc2dd63eca39efe78bf8f52
tannerkrewson <tannerkrewson@gmail.com>
2020-11-23 19:58:08 -0600

    prevent rare errors from crashing server

--- server/routes/socketio.js
+++ server/routes/socketio.js
@@ -2,128 +2,18 @@ module.exports = function(app) {
 	var dp = app.drawphone;
 	var stripTags = require("striptags");
 
-	app.io.on("connection", function(socket) {
+	app.io.on("connection", socket => {
 		var thisGame;
 		var thisUser;
 
-		socket.on("joinGame", onJoinGame);
+		const safeSon = (event, action) =>
+			socket.on(event, (...params) => {
+				try {
+					action(...params);
+				} catch (e) {}
+			});
 
-		socket.on("newGame", function(data) {
-			if (dp.locked) {
-				sendLockedError(socket, dp.minutesUntilRestart);
-				return;
-			}
-
-			var theName = stripTags(data.name);
-			if (theName.length > 2 && theName.length <= 16) {
-				thisGame = dp.newGame();
-				thisUser = thisGame.addPlayer(theName, socket);
-				socket.emit("joinGameRes", {
-					success: true,
-					game: thisGame.getJsonGame(),
-					you: thisUser.getJson()
-				});
-			} else {
-				socket.emit("joinGameRes", {
-					success: false,
-					error: "Name too short/long"
-				});
-			}
-		});
-
-		socket.on("tryStartGame", function(data) {
-			if (!thisUser || !thisGame) return;
-
-			if (dp.locked) {
-				sendLockedError(socket, dp.minutesUntilRestart);
-				return;
-			}
-
-			if (data.timeLimit !== false && thisUser.isHost) {
-				thisGame.startNewRound(
-					data.timeLimit,
-					data.wordPackName,
-					data.showNeighbors
-				);
-			}
-		});
-
-		socket.on("tryReplacePlayer", function(data) {
-			const thisRound = thisGame.currentRound;
-
-			if (!thisGame || !thisRound) return;
-
-			const toReplaceId = data.playerToReplace.id;
-
-			if (thisUser && thisRound.canBeReplaced(toReplaceId)) {
-				thisUser = thisRound.replacePlayer(
-					toReplaceId,
-					thisUser,
-					thisGame.code
-				);
-				thisGame.initPlayer(thisUser);
-				thisRound.updateWaitingList();
-				thisRound.nextLinkIfEveryoneIsDone();
-			} else {
-				thisRound.sendUpdateToPotentialPlayers(thisGame.code);
-			}
-		});
-
-		socket.on("kickPlayer", function(data) {
-			if (!thisGame || !thisUser) return;
-
-			var idToKick = data.playerToKick.id;
-			var playerToKick = thisGame.getPlayer(idToKick);
-			if (thisUser.isHost && playerToKick) {
-				//this will simulate the 'disconnect' event, and run all of the
-				//	methods that were tied into that in the initPlayer function
-				playerToKick.socket.disconnect();
-			}
-		});
-
-		socket.on("replacePlayerWithBot", function(data) {
-			const isInGame = thisGame && thisGame.currentRound;
-			const isHost = thisUser && thisUser.isHost;
-			if (!isInGame || !isHost) return;
-
-			const oldPlayer = data.playerToReplaceWithBot;
-			const botPlayer = thisGame.newBotPlayer(
-				"👻 The Ghost of " + oldPlayer.name
-			);
-			const thisRound = thisGame.currentRound;
-
-			if (botPlayer && thisRound.canBeReplaced(oldPlayer.id)) {
-				thisRound.replacePlayer(oldPlayer.id, botPlayer);
-				thisRound.updateWaitingList();
-				thisRound.nextLinkIfEveryoneIsDone();
-			}
-		});
-
-		socket.on("hostUpdatedSettings", function(setting) {
-			if (!thisGame || !thisUser) return;
-
-			if (thisUser.isHost) {
-				thisGame.sendUpdatedSettings(setting);
-			}
-		});
-
-		socket.on("addBotPlayer", function() {
-			if (!thisGame || !thisUser) return;
-
-			if (thisUser.isHost) {
-				thisGame.addBotPlayer();
-			}
-		});
-
-		socket.on("removeBotPlayer", function() {
-			if (!thisGame || !thisUser) return;
-
-			if (thisUser.isHost) {
-				thisGame.removeBotPlayer();
-			}
-		});
-
-		function onJoinGame(data) {
+		safeSon("joinGame", data => {
 			thisGame = dp.findGame(data.code);
 			var theName = stripTags(data.name);
 			if (!thisGame) {
@@ -151,7 +41,122 @@ module.exports = function(app) {
 					thisRound.sendUpdateToPotentialPlayers(thisGame.code);
 				}
 			}
-		}
+		});
+
+		safeSon("newGame", data => {
+			if (dp.locked) {
+				sendLockedError(socket, dp.minutesUntilRestart);
+				return;
+			}
+
+			var theName = stripTags(data.name);
+			if (theName.length > 2 && theName.length <= 16) {
+				thisGame = dp.newGame();
+				thisUser = thisGame.addPlayer(theName, socket);
+				socket.emit("joinGameRes", {
+					success: true,
+					game: thisGame.getJsonGame(),
+					you: thisUser.getJson()
+				});
+			} else {
+				socket.emit("joinGameRes", {
+					success: false,
+					error: "Name too short/long"
+				});
+			}
+		});
+
+		safeSon("tryStartGame", data => {
+			if (!thisUser || !thisGame) return;
+
+			if (dp.locked) {
+				sendLockedError(socket, dp.minutesUntilRestart);
+				return;
+			}
+
+			if (data.timeLimit !== false && thisUser.isHost) {
+				thisGame.startNewRound(
+					data.timeLimit,
+					data.wordPackName,
+					data.showNeighbors
+				);
+			}
+		});
+
+		safeSon("tryReplacePlayer", data => {
+			const thisRound = thisGame.currentRound;
+
+			if (!thisGame || !thisRound) return;
+
+			const toReplaceId = data.playerToReplace.id;
+
+			if (thisUser && thisRound.canBeReplaced(toReplaceId)) {
+				thisUser = thisRound.replacePlayer(
+					toReplaceId,
+					thisUser,
+					thisGame.code
+				);
+				thisGame.initPlayer(thisUser);
+				thisRound.updateWaitingList();
+				thisRound.nextLinkIfEveryoneIsDone();
+			} else {
+				thisRound.sendUpdateToPotentialPlayers(thisGame.code);
+			}
+		});
+
+		safeSon("kickPlayer", data => {
+			if (!thisGame || !thisUser) return;
+
+			var idToKick = data.playerToKick.id;
+			var playerToKick = thisGame.getPlayer(idToKick);
+			if (thisUser.isHost && playerToKick) {
+				//this will simulate the 'disconnect' event, and run all of the
+				//	methods that were tied into that in the initPlayer function
+				playerToKick.socket.disconnect();
+			}
+		});
+
+		safeSon("replacePlayerWithBot", data => {
+			const isInGame = thisGame && thisGame.currentRound;
+			const isHost = thisUser && thisUser.isHost;
+			if (!isInGame || !isHost) return;
+
+			const oldPlayer = data.playerToReplaceWithBot;
+			const botPlayer = thisGame.newBotPlayer(
+				"👻 The Ghost of " + oldPlayer.name
+			);
+			const thisRound = thisGame.currentRound;
+
+			if (botPlayer && thisRound.canBeReplaced(oldPlayer.id)) {
+				thisRound.replacePlayer(oldPlayer.id, botPlayer);
+				thisRound.updateWaitingList();
+				thisRound.nextLinkIfEveryoneIsDone();
+			}
+		});
+
+		safeSon("hostUpdatedSettings", setting => {
+			if (!thisGame || !thisUser) return;
+
+			if (thisUser.isHost) {
+				thisGame.sendUpdatedSettings(setting);
+			}
+		});
+
+		safeSon("addBotPlayer", () => {
+			if (!thisGame || !thisUser) return;
+
+			if (thisUser.isHost) {
+				thisGame.addBotPlayer();
+			}
+		});
+
+		safeSon("removeBotPlayer", () => {
+			if (!thisGame || !thisUser) return;
+
+			if (thisUser.isHost) {
+				thisGame.removeBotPlayer();
+			}
+		});
 	});
 };
 

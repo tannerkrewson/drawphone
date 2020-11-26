90e35c72e8f8d52a1b7f2a76e43759c4aef362f5
Tanner Krewson <tannerkrewson@gmail.com>
2020-11-26 08:01:16 -0600

    quick api and msg update

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,73 +1,61 @@
 const webpackAssets = require("../../webpack-assets.json");
 
-module.exports = function(app) {
+module.exports = function (app) {
 	var dp = app.drawphone;
 	var allPackNames = require("../app/words").getAllPackNames();
 	var safePackNames = require("../app/words").getAllPackNames(true);
 
-	app.get("/", function(req, res) {
+	app.get("/", function (req, res) {
 		const isSafeForWorkURL = req.headers.host.startsWith("dpk");
 		const wordpacks = isSafeForWorkURL ? safePackNames : allPackNames;
 
 		res.render("index", {
 			wordpacks,
 			js: webpackAssets.main.js,
-			css: webpackAssets.main.css
+			css: webpackAssets.main.css,
 		});
 	});
 
-	app.get("/how-to-play", function(req, res) {
+	app.get("/how-to-play", function (req, res) {
 		res.render("howtoplay", {
 			js: webpackAssets.main.js,
-			css: webpackAssets.main.css
+			css: webpackAssets.main.css,
 		});
 	});
 
-	app.get("/screenshots", function(req, res) {
+	app.get("/screenshots", function (req, res) {
 		res.render("screenshots", {
 			js: webpackAssets.main.js,
-			css: webpackAssets.main.css
+			css: webpackAssets.main.css,
 		});
 	});
 
-	app.get("/more-games", function(req, res) {
+	app.get("/more-games", function (req, res) {
 		res.render("moregames", {
 			js: webpackAssets.main.js,
-			css: webpackAssets.main.css
+			css: webpackAssets.main.css,
 		});
 	});
 
-	app.get("/archive", function(req, res) {
+	app.get("/archive", function (req, res) {
 		res.render("archive", {
 			js: webpackAssets.main.js,
-			css: webpackAssets.main.css
+			css: webpackAssets.main.css,
 		});
 	});
 
-	app.get("/stats", function(req, res) {
+	app.get("/stats", function (req, res) {
 		var games = [];
 		let realPlayerCount = 0;
 		for (var game of dp.games) {
-			const real = game.players.length - game.botCount;
-			realPlayerCount += real;
-
-			const inProgressCounts = game.currentRound
-				? {
-						disconnected:
-							game.currentRound.disconnectedPlayers.length,
-						potential: game.currentRound.potentialPlayers.length
-				  }
-				: {};
+			const players = game.players.length - game.botCount;
+			realPlayerCount += players;
 
 			var strippedGame = {
-				players: {
-					real,
-					bot: game.botCount,
-					total: game.players.length,
-					...inProgressCounts
-				},
+				players,
 				roundsPlayed: game.currentRoundNum - 1,
-				lastAction: timeSince(game.timeOfLastAction)
+				lastAction: timeSince(game.timeOfLastAction),
+				inProgress: game.inProgress,
 			};
 
 			games.unshift(strippedGame);
@@ -82,15 +70,15 @@ module.exports = function(app) {
 			totalSocketClients: app.io.engine.clientsCount,
 			totalRealPlayers: realPlayerCount,
 			lastReboot: timeSince(lastRebootDate),
-			games: games
+			games: games,
 		});
 	});
 
-	app.get("/admin", function(req, res) {
+	app.get("/admin", function (req, res) {
 		res.render("admin");
 	});
 
-	app.post("/lock", function(req, res) {
+	app.post("/lock", function (req, res) {
 		if (!process.env.ADMIN_PASSWORD) {
 			res.status(501).end();
 		}
@@ -103,7 +91,7 @@ module.exports = function(app) {
 		}
 	});
 
-	app.post("/new", function(req, res) {
+	app.post("/new", function (req, res) {
 		if (dp.locked) {
 			// 423 Locked
 			return res.status(423).send({ minutes: dp.minutesUntilRestart });
@@ -114,11 +102,11 @@ module.exports = function(app) {
 	});
 
 	if (app.get("env") === "development") {
-		app.get("/dev", function(req, res) {
+		app.get("/dev", function (req, res) {
 			res.render("index", {
 				wordpacks: allPackNames,
 				js: webpackAssets.main.js,
-				css: webpackAssets.main.css
+				css: webpackAssets.main.css,
 			});
 		});
 	}
